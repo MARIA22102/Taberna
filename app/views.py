548f6c572c53ae369e1990ad07d1e9bda7cfd9b3
MARIA22102 <mariajoannamaeramos@gmail.com>
2024-10-18 21:35:55 +0800

    *commit*

---
+++ app/views.py
@@ -0,0 +1,14 @@
+from django.shortcuts import render
+from django.views.generic import TemplateView
+
+class HomePageView(TemplateView):
+    template_name = 'app/home.html'
+
+class AboutPageView(TemplateView):
+    template_name = 'app/about.html'
+
+class ContactPageView(TemplateView):
+    template_name = 'app/contact.html'
+
+class ServicesPageView(TemplateView):
+    template_name = 'app/services.html'
